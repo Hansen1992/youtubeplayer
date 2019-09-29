26bd84420be72cf53b1e6cabf579a1a1d3c64b00
Martin Nøhr Åskov Hansen <noehraaskov@gmail.com>
2019-09-29 17:54:02 +0200

    Updates

---
+++ precache-manifest.9906e28041baa5c468d5ca1c944cb5af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e8e02a85d522cc549a9c45aa561b449",
+    "url": "/youtubeplayer/index.html"
+  },
+  {
+    "revision": "5832c8df5033015be5b5",
+    "url": "/youtubeplayer/static/css/main.03c6ca28.chunk.css"
+  },
+  {
+    "revision": "3bb9d65407740ba519c0",
+    "url": "/youtubeplayer/static/js/2.4f89ca66.chunk.js"
+  },
+  {
+    "revision": "5832c8df5033015be5b5",
+    "url": "/youtubeplayer/static/js/main.ee4c581e.chunk.js"
+  },
+  {
+    "revision": "0525eeaee74b4273d934",
+    "url": "/youtubeplayer/static/js/runtime-main.9f3a12e0.js"
+  }
+]);
