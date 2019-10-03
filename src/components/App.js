d5360131d5ce91721104e6a9a413fe75b65856e3
Martin Nøhr Åskov Hansen <50966456+Hansen1992@users.noreply.github.com>
2019-10-03 21:35:53 +0200

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -8,18 +8,16 @@ class App extends React.Component {
     state = { videos: [], selectedVideo: null};
     
     componentDidMount() {
-        // create the standard search term that appears on screen when opening the app
         this.onTermSubmit('Reactjs')
     }
 
     onTermSubmit = async term => {
         const response = await youtube.get('search', {
-            // it is called 'q' because that is what the youtube api documentation specifically told us to call it. You'll get an error if you write 'query' or something like that.
             params: { q: term
             }
         });
 
-        // make sure to update both items and the video player when searching on things
+        
         this.setState({
             videos: response.data.items,
         selectedVideo: response.data.items[0]});
@@ -30,10 +28,6 @@ class App extends React.Component {
     }
     render() {
         return (
-            // ui conatiner is the ui css that makes some margin to both left and right, making the searchbar centered
-            // Det er vigtigt at VideoDetail har 'video' og 
-            // VideoList har 'videos', da computeren ikke kan finde ud af det
-            // hvis vi skriver forskellige kommandoer i noget, der kun har ét formål
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
                 <div className="ui grid">
