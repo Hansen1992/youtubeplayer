a53b2ddc1879543d984c06418a914aecfb855501
Martin Nøhr Åskov Hansen <50966456+Hansen1992@users.noreply.github.com>
2019-10-03 21:36:46 +0200

    Update VideoDetail.js

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,9 +5,8 @@ const VideoDetail = ({ video }) => {
         return <div>Loading...</div>
     }
 
-    //fetching the data, he tell you in episode 126 how to do it.
     const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`
-    // the iframe makes it possible to get a videoplayer inside our app
+    
     return (
         <div>
             <div className="ui embed">
